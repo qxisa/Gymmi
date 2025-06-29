b983d7c46f7962574b160a36a63a51d879f810e3
Abdalla Maher Bakr <A.BAKR@nu.edu.eg>
2025-06-29 14:01:32 +0300

    script.js

--- script.js
+++ script.js
@@ -6,21 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const workouts = {
     Monday: [
-      { name: "Barbell Squats", img: "images/squat.jpg", video: "https://www.youtube.com/watch?v=Dy28eq2PjcM", calories: 100 },
-      { name: "Bench Press", img: "images/bench.jpg", video: "https://www.youtube.com/watch?v=gRVjAtPip0Y", calories: 80 }
+      { name: "Barbell Squats", img: "images/squat.jpg", video: "https://www.youtube.com/watch?v=Dy28eq2PjcM", sets:4 , reps:6 },
+      { name: "Barbell Bench Press", img: "images/bench.jpg", video: "https://www.youtube.com/watch?v=gRVjAtPip0Y", sets:4 , reps:6  },
+      { name: "Lat Pulldown", img: "images/latpulldown.jpg", video: "https://www.youtube.com/watch?v=CAwf7n6Luuc", sets:4 , reps:8  },
+      { name: "Seated Row", img: "images/seatedrow.jpg", video: "https://www.youtube.com/watch?v=GZbfZ033f74", sets:3 , reps:10  },
+      { name: "Dumbbell Shoulder Press", img: "images/shoulderpress.jpg", video: "https://www.youtube.com/watch?v=qEwKCR5JCog", sets:3 , reps:10 },
+      { name: "Plank", img: "images/plank.jpg", video: "https://www.youtube.com/watch?v=B296mZDhrP4", sets:3 , reps:30 "sec"  }
     ],
     Tuesday: [
-      { name: "Cable Flies", img: "images/cablefly.jpg", video: "https://www.youtube.com/watch?v=eozdVDA78K0", calories: 70 }
+      { name: "Chest Press", img: "images/chestpress.jpg", video: "https://www.youtube.com/watch?v=EVt3eG3Dt6w", sets:3 , reps:12  },
+      { name: "Cable Flies", img: "images/cablefly.jpg", video: "https://www.youtube.com/watch?v=eozdVDA78K0", sets:3 , reps:15  },
+      { name: "Lateral Raise", img: "images/lateralraise.jpg", video: "https://www.youtube.com/watch?v=kDqklk1ZESo", sets:3 , reps:15  },
+      { name: "Bicep Curls", img: "images/bicepcurl.jpg", video: "https://www.youtube.com/watch?v=ykJmrZ5v0Oo", sets:3 , reps:12  },
+      { name: "Tricep Pushdowns", img: "images/triceppushdown.jpg", video: "https://www.youtube.com/watch?v=2-LAMcpzODU", sets:3 , reps:15  },
+      { name: "Incline Walk", img: "images/treadmill.jpg", video: "https://www.youtube.com/watch?v=QJ5xNKn9nBU"  }
     ],
     Wednesday: [
-      { name: "Deadlift", img: "images/deadlift.jpg", video: "https://www.youtube.com/watch?v=op9kVnSso6Q", calories: 120 }
-    ],
-    Thursday: [], // Yoga day
-    Friday: [],   // Rest/stretch day
-    Saturday: [
-      { name: "Lat Pulldown", img: "images/latpulldown.jpg", video: "https://www.youtube.com/watch?v=CAwf7n6Luuc", calories: 90 }
-    ],
-    Sunday: []    // Yoga again
+      { name: "Deadlift", img: "images/deadlift.jpg", video: "https://www.youtube.com/watch?v=op9kVnSso6Q", sets:4 , reps:5  },
+      { name: "Incline Dumbbell Press", img: "images/inclinepress.jpg", video: "https://www.youtube.com/watch?v=8iPEnn-ltC8", sets:3 , reps:8  },
+      { name: "Pull-Ups", img: "images/pullup.jpg", video: "https://www.youtube.com/watch?v=eGo4IYlbE5g", sets:3 , reps:5  },
+      { name: "Seated Shoulder Press", img: "images/seatedshoulder.jpg", video: "https://www.youtube.com/watch?v=B-aVuyhvLHU", sets:3 , reps:10  },
+      { name: "Cable Row", img: "images/cablerow.jpg", video: "https://www.youtube.com/watch?v=pYcpY20QaE8", sets:3 , reps:12  },
+      { name: "Hanging Knee Raises", img: "images/kneeraises.jpg", video: "https://www.youtube.com/watch?v=JB2oyawG9KI", sets:3 , reps:15  }
+    ]
   };
 
   const yogaSessions = [
@@ -82,6 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
       card.appendChild(label);
       card.innerHTML += `
         <img src="${exercise.img}" alt="${exercise.name}" />
+        <p><strong>Sets:</strong> ${exercise.sets || 3}, <strong>Reps:</strong> ${exercise.reps || 10}</p>
         <p><a href="${exercise.video}" target="_blank">Watch Tutorial</a></p>
       `;
       container.appendChild(card);
